d78860389226537e5a24221aa8b6d43860fc3cb2
aball6412 <aaron.thompkins12@gmail.com>
2016-06-22 21:49:01 -0400

    more set up

--- server.js
+++ server.js
@@ -5,19 +5,32 @@ var app = express();
 //Get the port for either development or production
 var port = process.env.PORT || 3000;
 
-//Serve the static pages
+//Serve the static pages and set view engine
 app.use("/",express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
 
 
 
+
+
+//Serve response for home page hits
 app.get("/", function(request, response) {
     
     
     console.log("Got request");
-    response.send("Hello World");
+    response.render("index");
     
 });
 
 
 
+
+
+
+
 app.listen(port);
+
+
+
+
+
