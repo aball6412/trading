d7d744da250b8ee9985ea61e448a51b05d8e14a9
aball6412 <aaron.thompkins12@gmail.com>
2016-06-23 23:50:43 -0400

    displayed candle OHLC data to client

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var https = require("https");
 
 
+
 //Get the port for either development or production
 var port = process.env.PORT || 3000;
 
@@ -19,49 +20,80 @@ app.get("/", function(request, response) {
     
     
     //Set up HTTPS request to Oanda's API
+    var accountId = process.env.OANDA_ACCOUNTID;
+    var token = process.env.OANDA_TOKEN;
     
+    //Options for streaming rates
+//    var options = 
+//        {
+//            host: "stream-fxpractice.oanda.com",
+//            path: "/v1/prices?accountId=" + accountId + "&instruments=EUR_USD",
+//            method: "GET",
+//            headers: { "Authorization": "Bearer " + token }
+//        
+//        };
+    
+    //Options for one time candlestick rates
     var options = 
         {
-            host: "stream-fxpractice.oanda.com",
-            path: "/v1/prices?accountId=" + process.env.OANDA_ACCOUNTID + "&instruments=EUR_USD",
+            host: "api-fxtrade.oanda.com",
+            path: "/v1/candles?instrument=EUR_USD&count=5&granularity=M5",
             method: "GET",
-            headers: { "Authorization": "Bearer " + process.env.OANDA_TOKEN }
-        
+            headers: { "Authorization": "Bearer " + token }
         };
         
+    
+    
     
 
     https.request(options, function(res) {
         
-        //console.log(res);
-        
         var str = "";
+        var price_list = [];
         
         res.on("data", function(chunk) {
  
-            var price = JSON.parse(chunk.toString());
-            console.log(price);
+            str += chunk;
+           
         });
         
         res.on("end", function() {
 
-            console.log(str);
+            var price = JSON.parse(str).candles;
+            
+            for (var i in price) {
+                
+                var open = price[i].openBid;
+                var high = price[i].highBid;
+                var low = price[i].lowBid;
+                var close = price[i].closeBid;
+                
+                var display = 
+                    {
+                        open: open,
+                        high: high,
+                        low: low,
+                        close: close
+                    };
+                
+                price_list.push(display);
+                
+            } //End for statement
+            
+            
+            response.render("index", { candles: price_list });
+            
+            
         });
+        
+        
         
         
     
     }).end();
       
         
-    
 
-
-    
-    
-    
-    console.log("Got request");
-    response.render("index");
-    
 });
 
 
